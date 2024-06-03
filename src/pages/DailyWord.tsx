b161c578b7b193a599f404c655d93df6193a455d
Jo-Jae <whwodjs11@naver.com>
2024-06-03 18:01:12 +0900

    AccordionButton

--- src/pages/DailyWord.tsx
+++ src/pages/DailyWord.tsx
@@ -5,16 +5,16 @@ import {
   AccordionItem,
   AccordionPanel,
   Box,
+  Button,
   Flex,
   Text,
 } from "@chakra-ui/react";
 import { FC, useEffect } from "react";
-import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { FiVolume2 } from "react-icons/fi";
 
 const DailyWord: FC = () => {
   const navigate = useNavigate();
-
-  const { day } = useParams();
 
   const { state } = useLocation();
   const { wordData }: { wordData: IWords } = state;
@@ -38,25 +38,26 @@ const DailyWord: FC = () => {
       <Text fontSize={24} fontWeight="bold" textAlign="center" mt={8}>
         <Text display="inline-block" fontWeight="bold">
           Day {wordData.day}
-        </Text>{" "}
+        </Text>
         - {wordData.title}
       </Text>
-      <Accordion allowMultiple>
-        <Accordion mt={8} allowMultiple>
-          {wordData.sentences.map((v, i) => (
-            <AccordionItem key={i}>
-              <h2>
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    {v.english}
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>{v.korean}</AccordionPanel>
-            </AccordionItem>
-          ))}
-        </Accordion>
+      <Accordion mt={8} allowMultiple>
+        {wordData.sentences.map((v, i) => (
+          <AccordionItem key={i}>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left">
+                  {v.english}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+              <Button variant="ghost" size="sm" mb={2} ml={2}>
+                <FiVolume2 />
+              </Button>
+            </h2>
+            <AccordionPanel pb={4}>{v.korean}</AccordionPanel>
+          </AccordionItem>
+        ))}
       </Accordion>
     </Flex>
   );
